2a9cfc44a503f66525dba210113b9daba3d2c7c3
ichubtou <msjj0809@naver.com>
2023-03-08 23:01:38 +0900

    init

---
+++ src/main/java/com/example/vpctest/VpctestApplication.java
@@ -0,0 +1,13 @@
+package com.example.vpctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpctestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpctestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vpctest/VpctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vpctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpctestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
